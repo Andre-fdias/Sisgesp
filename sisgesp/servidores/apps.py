acdf290c232366c62aedb410f6e39d010a80a110
Andre-fdias <andrefonsecadias21@gmail.com>
2025-11-28 21:20:59 -0300

    Primeiro commit

--- sisgesp/servidores/apps.py
+++ sisgesp/servidores/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class ServidoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sisgesp.servidores'
+    name = 'sisgesp.servidores'  # ← Caminho completo
     verbose_name = 'Gestão de Servidores'
